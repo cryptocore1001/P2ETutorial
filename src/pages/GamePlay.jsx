b6e5e060e8035c4d3f0a30db5d127052647fe06e
tomaswinner <crutugremido-9188@ac-cool.c4.fr>
2023-08-14 13:15:55 +0100

    One-time play now enabled

--- src/pages/GamePlay.jsx
+++ src/pages/GamePlay.jsx
@@ -1,18 +1,20 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { Header, Game, Chat } from '../components'
-import { getGame } from '../services/blockchain'
+import { getGame, getScores } from '../services/blockchain'
 import { useGlobalState } from '../store'
-import GameInfo from '../components/GameInfo'
 
 const GamePlay = () => {
   const { id } = useParams()
   const [game] = useGlobalState('game')
+  const [scores] = useGlobalState('scores')
+  const [connectedAccount] = useGlobalState('connectedAccount')
   const [loaded, setLoaded] = useState(false)
 
   useEffect(() => {
     const fetchData = async () => {
       await getGame(id)
+      await getScores(id)
       setLoaded(true)
     }
 
@@ -23,7 +25,12 @@ const GamePlay = () => {
     loaded && (
       <>
         <Header />
-        <Game game={game} />
+        <Game
+          game={game}
+          isPlayed={scores.some(
+            (score) => score.played && score.player == connectedAccount
+          )}
+        />
         <Chat gid={id} />
       </>
     )
